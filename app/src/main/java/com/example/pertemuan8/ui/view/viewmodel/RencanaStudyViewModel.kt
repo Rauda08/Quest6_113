ce3a4312571e47569cc7b4ed760b3ac58af68ebf
Rauda08 <raudajnnh08@gmail.com>
2024-11-22 20:58:08 +0700

    menambahkan fitur untuk memilih matakuliah

---
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuan8.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8.ui.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel : ViewModel(){
+    private val _krsState = MutableStateFlow(RencanaStudi())
+
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
+
