6e450886bdfada99394324d7db8882628a672c66
Rauda08 <raudajnnh08@gmail.com>
2024-11-22 21:25:58 +0700

    membuat kelas mahasiswa view model

---
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.pertemuan8.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8.ui.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+    // Request
+    private val _mahasiswaUiState = MutableStateFlow(Mahasiswa())
+    // Response
+    val mahasiswaUiState: StateFlow<Mahasiswa> = _mahasiswaUiState.asStateFlow()
+
+    
