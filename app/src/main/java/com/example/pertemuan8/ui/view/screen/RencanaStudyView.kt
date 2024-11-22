c0d1f8903943741e74f8a688ce52b51ba2abd37f
Rauda08 <raudajnnh08@gmail.com>
2024-11-22 21:29:19 +0700

    menambahkan column, text, dropdown, checkbox

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
@@ -55,3 +55,154 @@ fun RencanaStudyView(
     }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+            Spacer(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(
+                            topEnd = 15.dp,
+                            topStart = 15.dp
+                        )
+                    )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = "Pilih Mata Kuliah Peminatan",
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Silahkan pilih mata kuliah yang anda inginkan",
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Light
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .padding(8.dp)
+                    )
+                    DynamicSelectedField(
+                        selectedValue = chosenDropdown,
+                        options = MataKuliah.options,
+                        label = "Mata Kuliah",
+                        onValueChangedEvent = {
+                            chosenDropdown = it
+                        }
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .padding(8.dp)
+                    )
+                    HorizontalDivider(
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = "Pilih Kelas Belajar",
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Silahkan pilih kelas dari mata kuliah yang Anda inginkan",
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Light
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .padding(8.dp)
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        RuangKelas.kelas.forEach { data ->
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                RadioButton(
+                                    selected = pilihanKelas == data,
+                                    onClick = { pilihanKelas = data }
+                                )
+                                Text(data)
+                            }
+                        }
+                    }
+                    Spacer(
+                        modifier = Modifier
+                            .padding(8.dp)
+                    )
+                    HorizontalDivider(
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .padding(8.dp)
+                    )
+                    Text(
+                        text = "Klausul Persetujuan Mahasiswa",
+                        fontWeight = FontWeight.Bold
+                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Checkbox(
+                            checked = checked,
+                            onCheckedChange = { checked = it },
+                            enabled = chosenDropdown.isNotBlank() && pilihanKelas.isNotBlank()
+                        )
+                        Text(
+                            text = "Saya menyetujui setiap pernyataan yang ada tanpa ada paksaan dari pihak manapun.",
+                            fontWeight = FontWeight.Light,
+                            fontSize = 10.sp
+                        )
+                    }
+
+
+                }
+            }
+        }
+    }
+}
